579700e9804c4645322c74039f38956b599f69b7
Bala Luguja <bluguja@gmail.com>
2020-10-15 06:08:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ class App < Sinatra::Base
   # Define your GET '/' route below and respond with "Hello, World!"
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+
 
   get '/' do 
     "Hello, World!"
