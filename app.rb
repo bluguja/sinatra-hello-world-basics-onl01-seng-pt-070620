932a5a38a26e18a61156025c0babf257d0221c47
Bala Luguja <bluguja@gmail.com>
2020-10-15 06:04:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+require_relative 'config/environment'
 
+class App < Sinatra::Base
+
+  get '/' do 
+    "Started my server using Shotgun! "
+  end
+
+end
 
 end
